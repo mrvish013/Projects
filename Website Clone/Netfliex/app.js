b1e78c50beef5d3e6b7931597ba61b75f484a632
mrvish013 <kharadevk013@gamial.com>
2024-08-22 16:45:12 +0530

    First commit

---
+++ Website Clone/Netfliex/app.js
@@ -0,0 +1,85 @@
+$(".box-contain").css({
+    width: "1150px",
+    fontSize: "25px",
+    textAlign: "justify",
+    color: "white",
+    margin: "0 auto",
+    border: "1px solid black",
+    backgroundColor: "rgb(45, 45, 45)",
+    padding: "20px",
+});
+$(".box-contain-1").css({
+    width: "1150px",
+    fontSize: "25px",
+    textAlign: "justify",
+    color: "white",
+    margin: "0 auto",
+    border: "1px solid black",
+    backgroundColor: "rgb(45, 45, 45)",
+    padding: "20px",
+});
+$(".box-contain-2").css({
+    width: "1150px",
+    fontSize: "25px",
+    textAlign: "justify",
+    color: "white",
+    margin: "0 auto",
+    border: "1px solid black",
+    backgroundColor: "rgb(45, 45, 45)",
+    padding: "20px",
+});
+$(".box-contain-3").css({
+    width: "1150px",
+    fontSize: "25px",
+    textAlign: "justify",
+    color: "white",
+    margin: "0 auto",
+    border: "1px solid black",
+    backgroundColor: "rgb(45, 45, 45)",
+    padding: "20px",
+});
+$(".box-contain-4").css({
+    width: "1150px",
+    fontSize: "25px",
+    textAlign: "justify",
+    color: "white",
+    margin: "0 auto",
+    border: "1px solid black",
+    backgroundColor: "rgb(45, 45, 45)",
+    padding: "20px",
+});
+$(".box-contain-5").css({
+    width: "1150px",
+    fontSize: "25px",
+    textAlign: "justify",
+    color: "white",
+    margin: "0 auto",
+    border: "1px solid black",
+    backgroundColor: "rgb(45, 45, 45)",
+    padding: "20px",
+});
+
+$(".bttn-1").click(function () {
+    $(".box-contain").slideToggle(1000);
+    console.log("Box is Hide");
+});
+$(".bttn-2").click(function () {
+    $(".box-contain-1").slideToggle(1000);
+    console.log("Box is Hide");
+});
+$(".bttn-3").click(function () {
+    $(".box-contain-2").slideToggle(1000);
+    console.log("Box is Hide");
+});
+$(".bttn-4").click(function () {
+    $(".box-contain-3").slideToggle(1000);
+    console.log("Box is Hide");
+});
+$(".bttn-5").click(function () {
+    $(".box-contain-4").slideToggle(1000);
+    console.log("Box is Hide");
+});
+$(".bttn-6").click(function () {
+    $(".box-contain-5").slideToggle(1000);
+    console.log("Box is Hide");
+});
